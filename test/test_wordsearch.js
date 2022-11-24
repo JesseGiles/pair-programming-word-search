f7a2f89756ef39ebbcf22d91349cc1dbbb4f7505
Jesse Giles <jesse.giles@telus.com>
2022-11-23 22:24:47 -0700

    fixed bugs in test_wordsearch.js, added additional functionality to wordsearch.js and ran additional testing

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,31 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return null if array is empty", function() {
+    const result = wordSearch([], 'NINJA');
+    assert.isNull(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'N', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'I', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'J', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'A', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'NINJA');
+
+    assert.isTrue(result);
+  });
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (letters.length === 0) {
+    return null;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  let verticalArrays = letters[0].length;
+  let verticalJoin = [];
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (let i = 0; i < verticalArrays; i++) {
+    verticalJoin.push([]);
+    for (let j = 0; j < letters.length; j++) {
+      verticalJoin[i].push(letters[j][i]);
+    }
+  }
+
+  for (let arrays of verticalJoin) {
+    let checkVerticalWord = arrays.join("");
+    if (checkVerticalWord.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
